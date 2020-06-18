b5fdf446ffb2dc340e0b936936ba1f4950b02da6
Luan Vinicius <nalucode@hotmail.com>
2020-06-18 00:46:01 -0300

    add map functio

---
+++ src/map/map.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const map = (array = [], func = (item) => item) => {
+  let counter = 0;
+  const mapInternal = (arrayInternal) => {
+    if (arrayInternal.length === 0) return [];
+
+    const [head, ...tail] = arrayInternal;
+    return [func(head, counter++, array)].concat(mapInternal(tail, func));
+  };
+  return mapInternal(array);
+};
+
+export default map;
