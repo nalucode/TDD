4512cf863f550244ae0b260a4562025ce9cac863
Luan Vinicius <nalucode@hotmail.com>
2020-06-22 23:08:30 -0300

    creation some method

---
+++ src/some/some.js
@@ -0,0 +1,12 @@
+const some = (array, func) => {
+  return (function someInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal;
+    return arrayInternal.length === 0
+      ? false
+      : func(head, counter, array)
+      ? true
+      : someInternal(tail, counter + 1);
+  })(array, 0);
+};
+
+export default some;
